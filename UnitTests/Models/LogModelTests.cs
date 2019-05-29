6c95a43a0275b344072814452e8d4e8d806e6cc4
Mike Koenig <mikekoenig@live.com>
2019-05-29 14:05:06 -0700

    Added Log Model Example Unit Tests

--- UnitTests/Models/LogModelTests.cs
+++ UnitTests/Models/LogModelTests.cs
@@ -19,5 +19,23 @@ namespace ModelUnitTests
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void LogModel_Update_Valid_Data_Should_Pass()
+        {
+
+            // Arange
+            var myTest = new LogModel();
+            var myUpdate = new LogModel
+            {
+                PhoneID = "Phone id"
+            };
+
+            // Act
+            var result = myTest.Update(myUpdate);
+
+            // Assert
+            Assert.AreEqual(myUpdate.PhoneID,myTest.PhoneID);
+        }
     }
 }
