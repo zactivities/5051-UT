f718e70e5cc9294c8744c374208d5f627eae62fd
Mike Koenig <mikekoenig@live.com>
2019-05-29 13:57:43 -0700

    Added LogModel Unit Test to project

---
+++ UnitTests/Models/LogModelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+
+namespace ModelUnitTests
+{
+    [TestClass]
+    public class LogModelUnitTests
+    {
+        [TestMethod]
+        public void LogModel_Default_Should_Pass()
+        {
+
+            // Arange
+
+            // Act
+            var result = new LogModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
