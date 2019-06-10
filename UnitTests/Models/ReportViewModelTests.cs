d3114036ddb98ba567e22671d47adcff4bc895d5
Zachary Bax <baxz@seattleu.edu>
2019-06-09 19:29:46 -0700

    Added ReportViewModelTests.cs to the project

---
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
