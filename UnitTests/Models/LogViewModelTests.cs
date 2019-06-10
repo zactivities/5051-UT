a0d2fead85dce7ad78697990dc1f785827f46d17
Zachary Bax <baxz@seattleu.edu>
2019-06-09 19:13:06 -0700

    Added LogViewModel Instantiate test case

--- UnitTests/Models/LogViewModelTests.cs
+++ UnitTests/Models/LogViewModelTests.cs
@@ -13,8 +13,9 @@ namespace UnitTests.Models
             // Arrange
 
             // Act
-
+            var result = new LogViewModel();
             // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
